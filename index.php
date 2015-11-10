89a9c3f1d18fe971b5a5d94643e0785ebe95d19a
doug <doug.burks@gmail.com>
2015-11-10 14:06:10 -0500

    remove Xplico and add ELSA

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 </center>
 <br> 
 <b>What is Security Onion?</b><br>
-Security Onion is a Linux distro for intrusion detection, network security monitoring, and log management. It's based on Ubuntu and contains Snort, Suricata, Sguil, Squert, Bro, NetworkMiner, Xplico, and many other security tools. The easy-to-use Setup wizard allows you to build an army of distributed sensors for your enterprise in minutes!<br>
+Security Onion is a Linux distro for intrusion detection, network security monitoring, and log management. It's based on Ubuntu and contains Snort, Suricata, Bro, OSSEC, Sguil, Squert, ELSA, NetworkMiner, and many other security tools. The easy-to-use Setup wizard allows you to build an army of distributed sensors for your enterprise in minutes!<br>
 <br>
 <b>How do I install and configure Security Onion?</b><br>
 Please follow the <a href="https://github.com/Security-Onion-Solutions/security-onion/wiki/Installation">Installation guides</a> on our <a href="https://github.com/Security-Onion-Solutions/security-onion/wiki">Wiki</a>.<br>
