fab80edaef2ad502547d287fdcdb047ad1e2bb87
doug <doug.burks@gmail.com>
2019-11-26 12:42:41 -0500

    update docs and cheat sheet for 16.04.6.3

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '16.04.6.2',
+    VERSION: '16.04.6.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
