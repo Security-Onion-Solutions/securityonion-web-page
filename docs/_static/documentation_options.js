c65ad411b86795a84f674caf45e10ac09321a4ad
doug <doug.burks@gmail.com>
2021-02-03 14:12:56 -0500

    Update docs and cheat sheet for 16.04.7.3 Security-Onion-Solutions/security-onion#1814

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '16.04.7.2',
+    VERSION: '16.04.7.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
